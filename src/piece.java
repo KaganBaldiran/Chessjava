b33715bddb076a874ee048896bfa5e474683f3b9
KaganBaldiran <k.baldiran@hotmail.com>
2023-04-19 00:12:42 +0200

    latest

--- src/piece.java
+++ src/piece.java
@@ -3,17 +3,15 @@ public abstract class piece {
     Math.Vec2<Integer> Coordinates;
     int Color;
 
-    piece()
+    piece(int x_cord, int y_cord , int color)
     {
-
-
-
-
+       this.Coordinates.SetValues(x_cord,y_cord);
+       this.Color = color;
     }
 
-
-
-
-
+    public abstract void Move(int newX,int newY);
+    public abstract void capture();
 
 }
+
+
