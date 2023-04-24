3e05457e8247d0cf7eed7529766e2c81125de0be
KaganBaldiran <k.baldiran@hotmail.com>
2023-04-24 03:10:56 +0200

    Collision checking

--- src/CollisionBox.java
+++ src/CollisionBox.java
@@ -56,12 +56,43 @@ public class CollisionBox
     boolean CheckCollisionBoxBox(CollisionBox Box_to_check)
     {
 
+        Math.Vec4<Float> cb1 = new Math.Vec4<>( (float)this.x + (this.width / 2) ,
+                                                (float)this.x - (this.width / 2) ,
+                                               (float)this.y + (this.height / 2) ,
+                                               (float)this.y - (this.height / 2));
+
+
+        Math.Vec4<Float> cb2 = new Math.Vec4<>( (float)Box_to_check.x + (Box_to_check.width / 2) ,
+                                                (float)Box_to_check.x - (Box_to_check.width / 2) ,
+                                                (float)Box_to_check.y + (Box_to_check.height / 2) ,
+                                                (float)Box_to_check.y - (Box_to_check.height / 2));
+
+
+        if ((cb1.x >= cb2.y && cb1.x <= cb2.x) || (cb1.y >= cb2.y && cb1.y <= cb2.x) || (cb2.x >= cb1.y && cb2.x <= cb1.x) || (cb2.y >= cb1.y && cb2.y <= cb1.x))
+        {
+            if ((cb1.z >= cb2.w && cb1.z <= cb2.w) || (cb1.w >= cb2.w && cb1.w <= cb2.z) || (cb2.z >= cb1.w && cb2.z <= cb1.z) || (cb2.w >= cb1.w && cb2.w <= cb1.z))
+            {
+
+                return true;
+
+            }
+
+        }
         return false;
     }
 
     boolean CheckCollisionBoxMouse(Math.Vec2<Double> MousePos)
     {
+        
+        if ((MousePos.x < (this.x + (this.width) / 2)) && (MousePos.x > (this.x - (this.width) / 2)))
+        {
+            if ((MousePos.y < (this.y + (this.height) / 2)) && (MousePos.y > (this.y - (this.height) / 2)))
+            {
 
+                return true;
+
+            }
+        }
         return false;
     }
 
