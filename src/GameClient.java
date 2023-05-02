d404b2d163efafe793c8a5185ceb5c80b9d3cf57
KaganBaldiran <k.baldiran@hotmail.com>
2023-05-02 18:56:41 +0200

    Network Improvements

--- src/GameClient.java
+++ src/GameClient.java
@@ -1,11 +1,12 @@
 import java.io.IOException;
 import java.net.*;
+import java.util.Objects;
 
 public class GameClient extends Thread
 {
 
     private InetAddress ipAddress;
-    private DatagramSocket socket;
+    private DatagramSocket clientsocket;
     private Game game;
 
     String DataTosend = new String("Is it coming?");
@@ -18,7 +19,7 @@ public class GameClient extends Thread
 
         try
         {
-            this.socket = new DatagramSocket();
+            this.clientsocket = new DatagramSocket();
             this.ipAddress = ipAddress;
         }
         catch (SocketException e)
@@ -32,20 +33,90 @@ public class GameClient extends Thread
     {
         while(true)
         {
+            try {
+                SendInfoToServer();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
 
-            byte[] data = new byte[1024];
+            /*byte[] data = new byte[1024];
             DatagramPacket packet = new DatagramPacket(data , data.length);
 
             try
             {
-                socket.receive(packet);
+                clientsocket.receive(packet);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
 
             String message = new String(packet.getData());
-            System.out.println("SERVER [" + packet.getAddress().getHostAddress() + " port: " + packet.getPort() +  "] > " + message);
+            System.out.println("SERVER [" + packet.getAddress().getHostAddress() + " port: " + packet.getPort() +  "] > " + message);*/
         }
+    }
+
+    public void SendInfoToServer() throws IOException {
+
+        DatagramSocket socket;
+        try {
+           socket = new DatagramSocket();
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+
+        byte[] senddata = "Hello".getBytes();
+        DatagramPacket sendpacket;
+
+        try {
+           sendpacket = new DatagramPacket(senddata, senddata.length, InetAddress.getByName(Objects.requireNonNull(GameServer.getIPv4Addresses(InetAddress.getAllByName("LocalHost"))).getHostAddress()), 7070);
+           this.clientsocket.send(sendpacket);
+        }
+        catch (RuntimeException | IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        DatagramPacket recievepacket;
+        try {
+            recievepacket = new DatagramPacket(new byte[1024], 1024);
+            this.clientsocket.receive(recievepacket);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String response = new String(recievepacket.getData());
+        String[] splitResponse = response.split("-");
+        InetAddress ip = InetAddress.getByName(splitResponse[0].substring(1));
+
+        int port = Integer.parseInt(splitResponse[1]);
+        System.out.println("IP: " + ip + "PORT: " + port);
+
+        int localPort = clientsocket.getLocalPort();
+        clientsocket.close();
+        try {
+            clientsocket = new DatagramSocket(localPort);
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+
+        clientsocket.setSoTimeout(1000);
+
+        for (int i = 0; i < 5000; i++) {
+
+            senddata = ("Datapacket(" + i + ")").getBytes();
+            sendpacket = new DatagramPacket(senddata, senddata.length,ip , port);
+            clientsocket.send(sendpacket);
+
+            try
+            {
+                recievepacket.setData(new byte[1024]);
+                clientsocket.receive(recievepacket);
+                System.out.println("REC: " + new String(recievepacket.getData()));
+            } catch (IOException e) {
+                System.out.println("SERVER TIMED OUT");
+            }
+
+        }
+        clientsocket.close();
     }
 
     public void SendData(byte[] data)
@@ -53,7 +124,7 @@ public class GameClient extends Thread
         DatagramPacket packet = new DatagramPacket(data , data.length , ipAddress , port);
         try
         {
-            socket.send(packet);
+            clientsocket.send(packet);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/GameServer.java
+++ src/GameServer.java
@@ -17,6 +17,10 @@ public class GameServer extends Thread
         try
         {
             this.socket = new DatagramSocket(port);
+            System.out.println("Waiting for client 1 on Port " + socket.getLocalPort());
+
+
+
 
         }
         catch (SocketException e)
@@ -30,7 +34,8 @@ public class GameServer extends Thread
     {
         while(true)
         {
-            byte[] data = new byte[1024];
+
+            /*byte[] data = new byte[1024];
             DatagramPacket packet = new DatagramPacket(data , data.length);
 
             try
@@ -45,6 +50,12 @@ public class GameServer extends Thread
             if(!message.isEmpty())
             {
                 SendData("Pong".getBytes(),packet.getAddress(), packet.getPort());
+            }*/
+
+            try {
+                HolePunching();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
 
         }
@@ -59,6 +70,64 @@ public class GameServer extends Thread
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+    }
+
+    public void HolePunching() throws IOException {
+        // Waiting for Connection of Client1 on Port 7070
+        // ////////////////////////////////////////////////
+
+        // open serverSocket on Port 7070
+        DatagramSocket serverSocket1 = new DatagramSocket(7070);
+
+        System.out.println("Waiting for Client 1 on Port "
+                + serverSocket1.getLocalPort());
+
+        // receive Data
+        DatagramPacket receivePacket = new DatagramPacket(new byte[1024], 1024);
+        serverSocket1.receive(receivePacket);
+
+        // Get IP-Address and Port of Client1
+        InetAddress IPAddress1 = receivePacket.getAddress();
+        int port1 = receivePacket.getPort();
+        String msgInfoOfClient1 = IPAddress1 + "-" + port1 + "-";
+
+        System.out.println("Client1: " + msgInfoOfClient1);
+
+        // Waiting for Connection of Client2 on Port 7071
+        // ////////////////////////////////////////////////
+
+        // open serverSocket on Port 7071
+        DatagramSocket serverSocket2 = new DatagramSocket(7071);
+
+        System.out.println("Waiting for Client 2 on Port "
+                + serverSocket2.getLocalPort());
+
+        // receive Data
+        receivePacket = new DatagramPacket(new byte[1024], 1024);
+        serverSocket2.receive(receivePacket);
+
+        // GetIP-Address and Port of Client1
+        InetAddress IPAddress2 = receivePacket.getAddress();
+        int port2 = receivePacket.getPort();
+        String msgInfoOfClient2 = IPAddress2 + "-" + port2 + "-";
+
+        System.out.println("Client2:" + msgInfoOfClient2);
+
+        // Send the Information to the other Client
+        // /////////////////////////////////////////////////
+
+        // Send Information of Client2 to Client1
+        serverSocket1.send(new DatagramPacket(msgInfoOfClient2.getBytes(),
+                msgInfoOfClient2.getBytes().length, IPAddress1, port1));
+
+        // Send Infos of Client1 to Client2
+        serverSocket2.send(new DatagramPacket(msgInfoOfClient1.getBytes(),
+                msgInfoOfClient1.getBytes().length, IPAddress2, port2));
+
+        //close Sockets
+        serverSocket1.close();
+        serverSocket2.close();
 
     }
 
