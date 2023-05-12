63256b9eec657c2001e8d7fd1098a1538c9ff15c
KaganBaldiran <k.baldiran@hotmail.com>
2023-05-12 03:08:20 +0200

    GameEvent initialization

---
+++ src/GameEvent.java
@@ -0,0 +1,31 @@
+public class GameEvent
+{
+    public final static int FIRST_PLAYER = 1;
+    public final static int SECOND_PLAYER = 2;
+    int Winner = 0;
+
+    Board GameBoard;
+
+    Player player1;
+    Player player2;
+
+
+    GameEvent()
+    {
+
+
+
+    }
+
+    public void GameLoop()
+    {
+
+
+    }
+
+    public void EndGame()
+    {
+
+    }
+    
+}
