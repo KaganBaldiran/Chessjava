6b498c6fdfe8b580b3785ef9ee5bcce595a6db07
KaganBaldiran <k.baldiran@hotmail.com>
2023-05-30 03:07:30 +0200

    Optimization

--- src/Game.java
+++ src/Game.java
@@ -113,148 +113,124 @@ public class Game extends JPanel implements Runnable
     @Override
     public void run()
     {
-
         while (isRunning)
         {
+            if(frame.isFocused()) {
 
-            if (ui.CreateGameButton.Pressed.IsMutexTrue())
-            {
-                Games.AddGameEvent(GameEventHandler.LAN_GAME_EVENT , true , ui);
+                if (ui.CreateGameButton.Pressed.IsMutexTrue()) {
+                    Games.AddGameEvent(GameEventHandler.LAN_GAME_EVENT, true, ui);
 
-                if (Games.<GameEvent.LANGameEvent>GetGameEvent(Games.GetGameEventCount() - 1).DeleteGameEvent.IsMutexTrue())
-                {
-                    Games.DeleteGameEvents();
-                }
-                else
-                {
-                    if(Games.<GameEvent.LANGameEvent>GetGameEvent(Games.GetGameEventCount() - 1).Server != null)
-                    {
-                        ui.ReadOnlyField.SetText(Games.<GameEvent.LANGameEvent>GetGameEvent(Games.GetGameEventCount() - 1).Server.GameLink);
-                        ui.ReadOnlyField.SetEditable(false);
+                    if (Games.<GameEvent.LANGameEvent>GetGameEvent(Games.GetGameEventCount() - 1).DeleteGameEvent.IsMutexTrue()) {
+                        Games.DeleteGameEvents();
+                    } else {
+                        if (Games.<GameEvent.LANGameEvent>GetGameEvent(Games.GetGameEventCount() - 1).Server != null) {
+                            ui.ReadOnlyField.SetText(Games.<GameEvent.LANGameEvent>GetGameEvent(Games.GetGameEventCount() - 1).Server.GameLink);
+                            ui.ReadOnlyField.SetEditable(false);
+                        }
+
+                        ui.JoinGameButton.setVisible(false);
+                        ui.CreateGameButton.setVisible(false);
                     }
 
-                    ui.JoinGameButton.setVisible(false);
-                    ui.CreateGameButton.setVisible(false);
+                    ui.CreateGameButton.Pressed.SetMutexFalse();
                 }
 
-                ui.CreateGameButton.Pressed.SetMutexFalse();
-            }
+                if (ui.JoinGameButton.Pressed.IsMutexTrue()) {
+                    Games.AddGameEvent(GameEventHandler.LAN_GAME_EVENT, false, ui);
 
-            if (ui.JoinGameButton.Pressed.IsMutexTrue())
-            {
-                Games.AddGameEvent(GameEventHandler.LAN_GAME_EVENT , false , ui);
+                    if (Games.<GameEvent.LANGameEvent>GetGameEvent(Games.GetGameEventCount() - 1).DeleteGameEvent.IsMutexTrue()) {
+                        Games.DeleteGameEvents();
+                    } else {
+                        ui.JoinGameButton.setVisible(false);
+                        ui.CreateGameButton.setVisible(false);
+                    }
 
-                if (Games.<GameEvent.LANGameEvent>GetGameEvent(Games.GetGameEventCount() - 1).DeleteGameEvent.IsMutexTrue())
-                {
-                    Games.DeleteGameEvents();
-                }
-                else
-                {
-                    ui.JoinGameButton.setVisible(false);
-                    ui.CreateGameButton.setVisible(false);
+                    ui.JoinGameButton.Pressed.SetMutexFalse();
                 }
 
-                ui.JoinGameButton.Pressed.SetMutexFalse();
-            }
-
-            if(Games.IsThereGame())
-            {
-                Games.<GameEvent.LANGameEvent>GetGameEvent(Games.GetGameEventCount() - 1).GameLoop();
-            }
-
-            BufferStrategy bufferstrategy = current_canvas.getBufferStrategy();
-            Graphics graphics = bufferstrategy.getDrawGraphics();
-
-
-
-            Graphics2D bufferedGraphics = bufferedImage.createGraphics();
-            bufferedGraphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-
-            graphics.setColor(Color.WHITE);
-            graphics.fillRect(0, 0 , current_canvas.getWidth(), current_canvas.getHeight());
-
-            float final_scale_coeffi = GraphicHandler.GetScreenScaleCoefficient(frame ,this.current_canvas, this.ScreenSize);
-
-
-            bufferedGraphics.setColor(RightComponentColor.darker());
-            bufferedGraphics.fillRect((int) (ui.UIsliderBar.UnchangingComponentSizes.x), 0, ui.UIsliderBar.UnchangingComponentSizes.y, (int) ScreenSize.getHeight());
-
-            bufferedGraphics.setColor(RightComponentColor);
-            bufferedGraphics.fillRoundRect((int) (ui.UIsliderBar.UnchangingComponentSizes.x + ((ui.UIsliderBar.UnchangingComponentSizes.y * 0.10f) / 2))  , 10, (int) (ui.UIsliderBar.UnchangingComponentSizes.y * 0.90f), (int) (bufferedImage.getHeight() * 0.980f),30,30);
-
-
-            if(Games.IsThereGame())
-            {
-                if (((GameEvent.LANGameEvent) Games.GetGameEvent(Games.GetGameEventCount() - 1)).Client.ConnectionState.equalsIgnoreCase("CONNECTED"))
-                {
-                    Games.<GameEvent.LANGameEvent>GetGameEvent(Games.GetGameEventCount() - 1).DrawGame(leftComponent.bufferedGraphics);
+                if (Games.IsThereGame()) {
+                    Games.<GameEvent.LANGameEvent>GetGameEvent(Games.GetGameEventCount() - 1).GameLoop();
                 }
-                else if (((GameEvent.LANGameEvent) Games.GetGameEvent(Games.GetGameEventCount() - 1)).Client.ConnectionState.equalsIgnoreCase("DISCONNECTED"))
-                {
+
+                BufferStrategy bufferstrategy = current_canvas.getBufferStrategy();
+                Graphics graphics = bufferstrategy.getDrawGraphics();
+
+
+                Graphics2D bufferedGraphics = bufferedImage.createGraphics();
+                bufferedGraphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                
+                graphics.setColor(Color.WHITE);
+                graphics.fillRect(0, 0, current_canvas.getWidth(), current_canvas.getHeight());
+
+                float final_scale_coeffi = GraphicHandler.GetScreenScaleCoefficient(frame, this.current_canvas, this.ScreenSize);
+
+                bufferedGraphics.setColor(RightComponentColor.darker());
+                bufferedGraphics.fillRect((int) (ui.UIsliderBar.UnchangingComponentSizes.x), 0, ui.UIsliderBar.UnchangingComponentSizes.y, (int) ScreenSize.getHeight());
+
+                bufferedGraphics.setColor(RightComponentColor);
+                bufferedGraphics.fillRoundRect((int) (ui.UIsliderBar.UnchangingComponentSizes.x + ((ui.UIsliderBar.UnchangingComponentSizes.y * 0.10f) / 2)), 10, (int) (ui.UIsliderBar.UnchangingComponentSizes.y * 0.90f), (int) (bufferedImage.getHeight() * 0.980f), 30, 30);
+
+                if (Games.IsThereGame()) {
+                    if (((GameEvent.LANGameEvent) Games.GetGameEvent(Games.GetGameEventCount() - 1)).Client.ConnectionState.equalsIgnoreCase("CONNECTED")) {
+                        Games.<GameEvent.LANGameEvent>GetGameEvent(Games.GetGameEventCount() - 1).DrawGame(leftComponent.bufferedGraphics);
+                    } else if (((GameEvent.LANGameEvent) Games.GetGameEvent(Games.GetGameEventCount() - 1)).Client.ConnectionState.equalsIgnoreCase("DISCONNECTED")) {
+                        bufferedGraphics.setColor(piece.TRANSPARENT_LIGHT_GRAY);
+                        bufferedGraphics.fillRect(0,
+                                (int) ((bufferedImage.getHeight() - ui.UIsliderBar.UnchangingComponentSizes.x) / 2),
+                                ui.UIsliderBar.UnchangingComponentSizes.x,
+                                ui.UIsliderBar.UnchangingComponentSizes.x);
+                    }
+                } else {
                     bufferedGraphics.setColor(piece.TRANSPARENT_LIGHT_GRAY);
                     bufferedGraphics.fillRect(0,
                             (int) ((bufferedImage.getHeight() - ui.UIsliderBar.UnchangingComponentSizes.x) / 2),
                             ui.UIsliderBar.UnchangingComponentSizes.x,
                             ui.UIsliderBar.UnchangingComponentSizes.x);
                 }
-            }
-            else
-            {
-                bufferedGraphics.setColor(piece.TRANSPARENT_LIGHT_GRAY);
-                bufferedGraphics.fillRect(0,
+
+                float scaledWidth = bufferedImage.getWidth() * final_scale_coeffi;
+                float scaledHeight = bufferedImage.getHeight() * final_scale_coeffi;
+
+                leftComponent.drawUIcomponent(bufferedGraphics,
+                        0,
                         (int) ((bufferedImage.getHeight() - ui.UIsliderBar.UnchangingComponentSizes.x) / 2),
                         ui.UIsliderBar.UnchangingComponentSizes.x,
-                        ui.UIsliderBar.UnchangingComponentSizes.x);
+                        ui.UIsliderBar.UnchangingComponentSizes.x,
+                        current_canvas);
+
+
+                FBO_position.SetValues((current_canvas.getWidth() / 2) - (scaledWidth / 2), current_canvas.getHeight() / 2 - (float) (ScreenSize.getHeight() * final_scale_coeffi) / 2);
+
+                if (Games.IsThereGame()) {
+                    Games.<GameEvent.LANGameEvent>GetGameEvent(Games.GetGameEventCount() - 1).UpdateBoardUtilities(ScreenSize, final_scale_coeffi, FBO_position, bufferedImage, ui);
+                } else {
+                    Board.UpdateSquareSize(ScreenSize.height * final_scale_coeffi);
+                    Board.UpdateSQUARESIZEUI(ui.UIsliderBar.ComponentSizes.z);
+                }
+
+                ui.setUIsize(((int) scaledWidth), (int) scaledHeight, ScreenSize.height * final_scale_coeffi, ScreenSize.height * final_scale_coeffi);
+
+                if (Games.IsThereGame()) {
+                    ConnectionState.SetPosition((int) (ScreenSize.width * 0.835f), 30);
+                    ConnectionState.DrawText(((GameEvent.LANGameEvent) Games.GetGameEvent(Games.GetGameEventCount() - 1)).Client.ConnectionState);
+                }
+
+                graphics.drawImage(bufferedImage, FBO_position.x.intValue(), FBO_position.y.intValue(), (int) scaledWidth, (int) scaledHeight, current_canvas);
+
+                ui.UpdateBoardAttribs(FBO_position, final_scale_coeffi, ScreenSize);
+                ui.Update();
+
+                ui.paintComponent(graphics);
+
+                bufferstrategy.show();
+                graphics.dispose();
+
+                bufferedGraphics.setBackground(LeftComponentColor);
+                bufferedGraphics.clearRect(0, 0, 3000, 4000);
+
+                frame.revalidate();
+                frame.repaint();
             }
-
-            float scaledWidth = bufferedImage.getWidth() * final_scale_coeffi;
-            float scaledHeight = bufferedImage.getHeight() * final_scale_coeffi;
-
-            leftComponent.drawUIcomponent(bufferedGraphics,
-                    0,
-                    (int)((bufferedImage.getHeight() -ui.UIsliderBar.UnchangingComponentSizes.x)/2) ,
-                    ui.UIsliderBar.UnchangingComponentSizes.x,
-                    ui.UIsliderBar.UnchangingComponentSizes.x,
-                    current_canvas);
-
-
-            FBO_position.SetValues((current_canvas.getWidth()/2) - (scaledWidth/2) , current_canvas.getHeight()/2 - (float)(ScreenSize.getHeight() * final_scale_coeffi)/2);
-
-            if(Games.IsThereGame())
-            {
-                Games.<GameEvent.LANGameEvent>GetGameEvent(Games.GetGameEventCount() - 1).UpdateBoardUtilities(ScreenSize, final_scale_coeffi, FBO_position, bufferedImage, ui);
-            }
-            else
-            {
-                Board.UpdateSquareSize(ScreenSize.height * final_scale_coeffi);
-                Board.UpdateSQUARESIZEUI(ui.UIsliderBar.ComponentSizes.z);
-            }
-
-            ui.setUIsize(((int) scaledWidth), (int) scaledHeight,ScreenSize.height * final_scale_coeffi,ScreenSize.height * final_scale_coeffi);
-
-            if(Games.IsThereGame())
-            {
-                ConnectionState.SetPosition((int) (ScreenSize.width * 0.835f), 30);
-                ConnectionState.DrawText(((GameEvent.LANGameEvent) Games.GetGameEvent(Games.GetGameEventCount() - 1)).Client.ConnectionState);
-            }
-
-            graphics.drawImage(bufferedImage, FBO_position.x.intValue(), FBO_position.y.intValue(), (int)scaledWidth, (int)scaledHeight, current_canvas);
-
-
-            ui.UpdateBoardAttribs(FBO_position, final_scale_coeffi , ScreenSize);
-            ui.Update();
-
-            ui.paintComponent(graphics);
-
-            bufferstrategy.show();
-            graphics.dispose();
-
-            bufferedGraphics.setBackground(LeftComponentColor);
-            bufferedGraphics.clearRect(0,0, 3000, 4000);
-
-            frame.revalidate();
-            frame.repaint();
         }
 
             System.out.println("Game Terminated");
